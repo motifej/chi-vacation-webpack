a691bc8754afa8e317530ca219c5ce353dac50de
Evgeny <ggevgg@gmail.com>
2016-04-25 18:14:11 +0300

    add authorization header

---
+++ src/app/core/constants/localstorage.consts.js
@@ -0,0 +1,1 @@
+export const USERSTORAGEKEY = 'vacationsUser';
--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,10 +1,14 @@
+import { USERSTORAGEKEY } from './core/constants/localstorage.consts';
 'use strict';
 
-function runBlock($log, $rootScope, permission) {
+function runBlock($log, $rootScope, permission, $http, $localStorage) {
 	'ngInject';
 
 	$rootScope.$on("$stateChangeStart", permission.init);
-	
+
+	let token = $localStorage[ USERSTORAGEKEY ].data.token;
+	if (token) 
+		$http.defaults.headers.common.Authorization = 'Bearer ' + token;
 }
 
 export default runBlock;
