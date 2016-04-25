9589def9f206cc288ddd0f8f11bd6b9ee294cabb
Evgeny <ggevgg@gmail.com>
2016-04-25 12:35:38 +0300

    some fix add users create vacations

--- src/app/core/services/firebaseResolver.provider.js
+++ src/app/core/services/firebaseResolver.provider.js
@@ -3,30 +3,19 @@ export default function (app) {
 
     function FirebaseResolverProvider () {
         this.loadUser = loadUser;
-        this.getUsersList = getUsersList;
         this.$get = () => this;
     }
 
-        function loadUser (firebaseService, $state, states, sailsService, sailsAuthService, $rootScope, actions) {
+        function loadUser ($state, states, sailsService, sailsAuthService, $rootScope, actions) {
             "ngInject";
             let user = sailsAuthService.getAuthUser().user;
             const { USERLOADED } = actions;
-            return sailsService.userResource.getUserData({id: user.id}).$promise.then( e => {
+            return sailsService.getUser({id: user.id}).then( e => {
                 $rootScope.$emit(USERLOADED, e.data);
                 return e;
             })
                     .catch( err =>
                         $state.go(states.ERRLOAD,{err: err}) );
-            /*return firebaseService
-                    .loadUser()
-                    .catch( err =>
-                        $state.go(states.ERRLOAD,{err: err}) );*/
-        }
-
-        function getUsersList(firebaseService) {
-            'ngInject';
-            debugger;
-            return firebaseService.getUsersList();
         }
 
 }
