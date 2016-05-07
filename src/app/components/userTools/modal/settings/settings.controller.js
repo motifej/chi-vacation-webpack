3c2dcda26d26b503f59c6a992fdd262f6fc4b464
Evgeny <ggevgg@gmail.com>
2016-05-07 22:53:32 +0300

    add settings

--- src/app/components/userTools/modal/settings/settings.controller.js
+++ src/app/components/userTools/modal/settings/settings.controller.js
@@ -1,64 +1,43 @@
 export default class SettingsController {
-  constructor ($filter, $uibModalInstance, settings, $uibModal, toastr, sailsService, groups, sailsAuthService, mailService) {
+  constructor ($uibModalInstance, settings, toastr, sailsService) {
     'ngInject';
 
     this.invalidForm = false;
-    this.namePattern = '[a-zA-Zа-яА-Я]+';
     this.emailPattern = '\\w+.?\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,6}';
-    this.filter = $filter;
     this.sailsService = sailsService;
-    this.mailService = mailService;
-    this.sailsAuthService = sailsAuthService;
     this.toastr = toastr;
-    this.modal = $uibModal;
     this.modalInstance = $uibModalInstance;
 
     this.settings = settings.data.data;
-    console.log(this.settings);
   }
 
   addEmail() {
-    this.sailsService.saveSettings([...this.settings.email, this.newEmail])
-    .then(
-      data => {
-        this.settings.email.push(this.newEmail);
+    if (this.newEmail)
+      if (~this.settings.email.indexOf(this.newEmail))
+        this.toastr.error('Duplicate email');
+      else {
+        this.settings.email.push(this.newEmail); 
         this.newEmail = '';
-        this.toastr.success('Email added')
       }
-    ),
-    e => this.toastr.error(e.message, 'Error adding email')
-
+    else
+      this.toastr.error('Incorrect email')
   }
 
   deleteEmail(email) {
-    this.sailsService.saveSettings(this.settings.email.filter(
+    this.settings.email = this.settings.email.filter(
       el => el !== email
-    ))
-      .then(
-        data => {
-          this.settings.email = this.settings.email.filter(
-            el => el !== email
-          );
-          this.toastr.success('Email deleted') 
-        }
-      ),
-      e => this.toastr.error(e.message, 'Error deleting email')
+    );
   }
 
   submitForm (isValid) {
-   /* if (isValid) {
-      this.invalidForm = false;
-      this.modalInstance.close();
-      delete this.newUser.vacations;
-      delete this.newUser.daysoff;
-      this.updateUser({id: this.newUser.id}, this.newUser).$promise.then(
-        () => this.toastr.success('Edit user success', 'Success'),
-        error => this.toastr.error(error.data.message, 'Error updating user')
-        );
-    } else {
-      this.toastr.error('Not all fields are filled', 'Error');
-      this.invalidForm = true;
-    }*/
+    this.sailsService.saveSettings(this.settings)
+      .then(
+        data => {
+          this.toastr.success('Settings were saved');
+          this.modalInstance.dismiss('cancel');
+        },
+        e => this.toastr.error(e.message, 'Error saving settings'))
+
   }
 
   cancel() {
