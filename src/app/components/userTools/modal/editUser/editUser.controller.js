9a38eff9d63247131b11a2b566f961439754f8f4
Evgeny <ggevgg@gmail.com>
2016-05-19 17:38:15 +0300

    fix user info

--- src/app/components/userTools/modal/editUser/editUser.controller.js
+++ src/app/components/userTools/modal/editUser/editUser.controller.js
@@ -51,7 +51,6 @@ export default class AddNewUserController {
     });
     modalInstance.result.then(
       selectedItem => {
-        debugger;
         if (selectedItem) {
           this.invalidForm = false;
           this.modalInstance.close();
