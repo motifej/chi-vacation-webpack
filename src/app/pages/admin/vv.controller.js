aeb685a138d9aaed0112d295affe90bd90f86cef
Prorock201 <miklebelyakov@gmail.com>
2016-06-17 18:22:58 +0300

    Bug #12873

--- src/app/pages/admin/vv.controller.js
+++ src/app/pages/admin/vv.controller.js
@@ -50,6 +50,10 @@ export default class VvController {
     this.sendingAdditional = false;
     this.sendingRequest = false;
 
+    $scope.$watch('newUser.role', function() {
+      debugger;
+    });
+
 
     
   }
