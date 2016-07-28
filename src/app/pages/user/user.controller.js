d4a764f2f23a5f115bbb6b0a102eeac60496734b
Evgeny <ggevgg@gmail.com>
2016-07-28 20:21:04 +0300

    refactor user ctrl

--- src/app/pages/user/user.controller.js
+++ src/app/pages/user/user.controller.js
@@ -7,6 +7,11 @@ import {  DAYSOFF,
           SHOW_VACATION,
           SHOW_DAYOFF,
           SHOW_WORKFROMHOME } from '../../core/constants/vacations.consts';
+import {  CONFIRMED, 
+          REJECTED, 
+          INPROGRESS,
+          NEW,
+          HISTORY } from '../../core/constants/status.consts';
 
 export default class UserController {
 
@@ -94,7 +99,7 @@ export default class UserController {
             .add(1, 'month')))*/
 
         user.vacations
-          .filter( item => item.year == (user.year - 1) && item.status != "rejected" )
+          .filter( item => item.year == (user.year - 1) && item.status != REJECTED )
           .forEach( item => user.spendPrevVacation += this.calcDays(item.startdate, item.enddate));
 
         user.availablePrevDays += this.calcAvailablePrevDays(vacationStartDate, user);
@@ -102,14 +107,14 @@ export default class UserController {
         user.availableDays += user.availablePrevDays;
       }
       user.vacations
-      .filter( item => item.year == user.year && item.status != "rejected" )
+      .filter( item => item.year == user.year && item.status != REJECTED )
       .forEach( item => user.spendVacation += this.calcDays(item.startdate, item.enddate));
 
       /*console.log(user.totalDays)*/
       user.availableCurDays += user.totalDays - user.spendVacation;
       user.availableDays += user.availableCurDays < 0 ? 0 : user.availableCurDays;
       user.daysoff
-      .filter( item => item.year == user.year && item.status != "rejected" )
+      .filter( item => item.year == user.year && item.status != REJECTED )
       .forEach( item => {
         user.spendDaysOff += this.calcDays( item.startdate, item.enddate);
       });
@@ -145,62 +150,22 @@ export default class UserController {
     return user;
   }
 
-  submitHandler(startDate, endDate) {
+  submitHandler(startdate, enddate) {
+    let vm = this;
     this.sending = true;
     let vac_type = this.getStatus(this.vacationState, true);
-    let vm = this;
-    let sDate = new Date(startDate).getTime();
-    let eDate = new Date(endDate).getTime();
-    //let toastrOptions = {progressBar: false};
-    
-    let listArray = [];
-    vm.vacations = [];
-    listArray.push(this.user['vacations']);
-    listArray.push(this.user['daysoff']);
-    listArray.push(this.user['workfromhome']);
-
-    listArray.forEach( list => {
-      if (list) {
-        for (let item in list) {
-          if (list[item].status === 'rejected') continue;
-          vm.vacations.push({startDate: list[item].startdate, endDate: list[item].enddate, status: list[item].status, commentary: list[item].commentary});
-        }
-      }
-    });
-
-    if (vm.vacations && isCrossingIntervals(vm.vacations)) {
-      this.toastr.error(vac_type + ' intervals are crossing! Please, choose correct date.');
-      this.sending = false;
-      return;
-    }
-
-    let total;
-    if (this.vacationState === this.VACATIONS) total = this.user.availableDays;
-    if (this.vacationState === this.DAYSOFF) total = this.user.availableDaysOff;
-    if (this.vacationState === this.WORKFROMHOME) total = this.user.availableWorkFromHome;
-//    let total = this.vacationState === this.VACATIONS ? this.user.availableDays : this.user.availableDaysOff;
-    if (this.user.vacationDays > total) {
-      this.toastr.error('You have exceeded the number of available days!');
-      this.sending = false;
-      return;
-    }
-
-    let vacation = {
-      startdate: new Date(sDate),
-      enddate: new Date(eDate),
-      //status: 'inprogress',
-      commentary: null,
-      status: "new"
-    };
-    const create = this.sailsService['create' + this.vacationState];
-
+    let sDate = new Date(startdate).getTime();
+    let eDate = new Date(enddate).getTime();
+    let status = NEW;
     const { id: uid, year } = this.user;
-    const { startdate, enddate, status } = vacation;
+    const create = this.sailsService['create' + this.vacationState];
+    
     const createError = ({data}) => {
-      this.sending = false; 
       this.toastr.error(this.$parse('raw.message')(data) || this.$parse('data.raw.message')(data) || '', 'Error creating ' + vac_type.toLowerCase())
-    };
-    const createSuccess = res => {
+      this.sending = false; 
+    }
+
+    const createSuccess = (res) => {
       this.toastr.success(vac_type + ' request was sent successfully!');
       if (!_.find(this.user[this.vacationState], {id:res.data.id}))
         this.user[this.vacationState].push(res.data);
@@ -208,13 +173,32 @@ export default class UserController {
       this.sending = false;
     }
 
+    //список новых и подтвержденных заявок
+    vm.vacations = this.combineVacations().filter( item => item.status !== REJECTED )
 
-    if(this.vacationState == "daysoff") {
+    //проверка на пересечение интервалов
+    if (isCrossingIntervals(vm.vacations)) {
+      this.toastr.error(vac_type + ' intervals are crossing! Please, choose correct date.');
+      this.sending = false;
+      return;
+    }
+
+    //проверка на количество доступных дней
+    let total = getAvailableDays(this.vacationState, this.user);
+    if (this.user.vacationDays > total) {
+      this.toastr.error('You have exceeded the number of available days!');
+      this.sending = false;
+      return;
+    }
+
+    //если дей-офф или работа из дому -> создаем заявку
+    if(this.vacationState == DAYSOFF || this.vacationState == WORKFROMHOME) {
       create({uid, startdate, enddate, status, year })
        .then(createSuccess, createError);
       return;
     }
 
+    //дополнительная проверка для отпуска
     if(this.user.availablePrevDays <= 0) {
       create({uid, startdate, enddate, status, year })
        .then(createSuccess, createError);
@@ -232,17 +216,22 @@ export default class UserController {
        .then(createSuccess, createError);
     }
 
-    function isCrossingIntervals(dateIntervals) {
-      if(dateIntervals.length === 0) return false;
+    function getAvailableDays(state, user) {
+      switch (state) {
+        case VACATIONS: return user.availableDays;
+        case DAYSOFF: return user.availableDaysOff;
+        case WORKFROMHOME: return user.availableWorkFromHome;
+      }
+    }
 
-      let result = dateIntervals.filter(function(item) {
-        if  (sDate <= new Date(item.endDate).getTime() && eDate >= new Date(item.startDate).getTime()) {
-          return true;
-        }
-      });
+    function isCrossingIntervals(dateIntervals) {
+      if(!dateIntervals || dateIntervals.length === 0) return false;
+
+      let result = dateIntervals.filter( (item) => 
+        (( vac_type === 'Work from home' || ( vac_type !== 'Work from home' && item.vac_type !== TYPE_WORKFROMHOME) ) && sDate <= new Date(item.enddate).getTime() && eDate >= new Date(item.startdate).getTime())
+      );
 
       return !!result.length;
-
     }
   }
 
@@ -286,7 +275,11 @@ export default class UserController {
   }
 
   combineVacations() {
-    return this.user.vacations.concat(this.user.daysoff, this.user.workfromhome);
+    return [].concat(
+      this.user[VACATIONS], 
+      this.user[DAYSOFF], 
+      this.user[WORKFROMHOME])
+    //return this.user.vacations.concat(this.user.daysoff, this.user.workfromhome);
   }
 
   getStatus(vac_type, capitalize) {
