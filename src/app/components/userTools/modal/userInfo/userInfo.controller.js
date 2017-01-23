c9a87e3a475b5f7599acfbd0492d8d48d96b3fc8
Pashkovich1 <maxim.chalenko.chisw.com>
2017-01-23 15:19:46 +0200

    Fix deleting users

--- src/app/components/userTools/modal/userInfo/userInfo.controller.js
+++ src/app/components/userTools/modal/userInfo/userInfo.controller.js
@@ -23,4 +23,30 @@ export default class UserInfoController {
         let days = moment().isoWeekdayCalc(this.user.employmentdate, new Date(),[1,2,3,4,5,6,7]);
         return Math.round((days % 365.25)*20/365.25);
     }
+    deleteUser () {
+      let modalInstance = this.modal.open({
+        templateUrl: require('!!file!../confirmDialog/confirmDialog.html'),
+        controller: require('../confirmDialog/confirmDialog.controller'),
+        controllerAs: 'confirm',
+        size: 'sm'
+      });
+      modalInstance.result.then(
+        selectedItem => {
+          if (selectedItem) {
+            this.invalidForm = false;
+            this.modalInstance.close();
+            delete this.newUser.vacations;
+            delete this.newUser.daysoff;
+            delete this.newUser.workfromhome;
+            this.updateUser({id: this.newUser.id}, angular.extend(this.newUser, {
+              deleted: true
+            })).$promise
+              .then(
+                () => this.toastr.success('Deleting user success', 'Success'),
+                error => this.toastr.error(error.data.message, 'Error updating user')
+              );
+          }
+        }
+      )
+    }
   }
